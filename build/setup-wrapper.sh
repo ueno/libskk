e50f68f7b0ba385cf18301edf603c7ef58af42f0
Daiki Ueno <ueno@gnu.org>
2026-02-02 11:31:34 +0900

    ci: Do not create user in setup-wrapper.sh
    The user should have been already created in the container image.

--- build/setup-wrapper.sh
+++ build/setup-wrapper.sh
@@ -4,15 +4,12 @@ set -ex
 
 mkdir "$GITHUB_WORKSPACE/$BUILDDIR"
 mkdir "$GITHUB_WORKSPACE/$INSTALLDIR"
-mkdir -p "$GITHUB_WORKSPACE/$INSTALLDIR/etc/pki/ca-trust-source"
-mkdir -p "$GITHUB_WORKSPACE/$INSTALLDIR/share/pki/ca-trust-source"
 
 if test $(id -u) -eq 0; then
     case "$RUNNER_OS" in
 	Linux)
-	    useradd -m "$RUNUSER"
 	    chown -R "$RUNUSER" "$GITHUB_WORKSPACE/$BUILDDIR"
-	    # This is necessary to put p11-kit.pot in $(srcdir)
+	    # This is necessary to put libskk.pot in $(srcdir)
 	    chown -R "$RUNUSER" "$GITHUB_WORKSPACE/po"
 	    ;;
 	*)
