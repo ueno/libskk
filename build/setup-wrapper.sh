b40cb23f01cc4795bc5b61e338ee439cf165c27f
Daiki Ueno <ueno@gnu.org>
2026-02-02 11:43:15 +0900

    ci: Give up on VPATH build
    It is hard to preserve the checkout clean with the current Vala
    tooling in Autotools. Let's not bother doing VPATH build.

--- build/setup-wrapper.sh
+++ build/setup-wrapper.sh
@@ -2,15 +2,11 @@
 
 set -ex
 
-mkdir "$GITHUB_WORKSPACE/$BUILDDIR"
-mkdir "$GITHUB_WORKSPACE/$INSTALLDIR"
-
 if test $(id -u) -eq 0; then
     case "$RUNNER_OS" in
 	Linux)
-	    chown -R "$RUNUSER" "$GITHUB_WORKSPACE/$BUILDDIR"
-	    # This is necessary to put libskk.pot in $(srcdir)
-	    chown -R "$RUNUSER" "$GITHUB_WORKSPACE/po"
+	    chown -R "$RUNUSER" "$GITHUB_WORKSPACE"
+	    mkdir "$GITHUB_WORKSPACE/$INSTALLDIR"
 	    ;;
 	*)
 	    echo "Unsupported OS: $RUNNER_OS" 1>&2
